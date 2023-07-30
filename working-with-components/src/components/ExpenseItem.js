fb889058a73aaa0f44ec8f734acdc0f51842fa6a
Ankush Mukherjee <ankush.mukherjee24@gmail.com>
2023-07-31 01:05:22 +0530

    add custom components

---
+++ working-with-components/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import "./ExpenseItem.css";
+
+export const ExpenseItem = () =>{
+    return (
+        <div className="expense-item">
+            <div className="">March 28th 2021</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price"> ₹ 294.67</div>
+            </div>
+        </div>
+    );
+}
